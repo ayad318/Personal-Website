30937b5cb4b80c8024bc1c282ecefb8a22881dff
ayad318 <ayad.farhat2001@icloud.com>
2023-10-17 09:10:07 +1100

    initial skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>About Me</h1>
+                <p>This is a brief description about me.</p>
+                <a href="http://">books</a>
+                <button onClick={() => {}}>Upload Resume</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
