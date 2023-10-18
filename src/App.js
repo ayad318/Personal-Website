667c4add8fd2196e178bf22bd418f958eeaa0a25
ayad318 <ayad.farhat2001@icloud.com>
2023-10-18 11:04:57 +1100

    added info

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>About Me</h1>
-                <p>This is a brief description about me.</p>
+                <h1>Ayad Farhat</h1>
+                <p>Birthday: March 6, 2001</p>
+                <p>Education: Bachelor's in Software Engineer with honours</p>
+                <p>Email: </p>
+                <p>Location: Sydney, Australia</p>
                 <a href="http://">books</a>
                 <button onClick={() => {}}>Upload Resume</button>
             </header>
-            <main>
-                <h2>Experience</h2>
-                <ul>
-                    <li>Company 1</li>
-                    <li>Company 2</li>
-                    <li>Company 3</li>
-                </ul>
-            </main>
         </div>
     );
 }
