d862d100195ad040c655dea0cfd9eb15c5e481f3
ayad318 <ayad.farhat2001@icloud.com>
2023-10-18 15:50:04 +1100

    new css

--- src/App.js
+++ src/App.js
@@ -2,16 +2,47 @@ import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Ayad Farhat</h1>
-                <p>Birthday: March 6, 2001</p>
-                <p>Education: Bachelor's in Software Engineer with honours</p>
-                <p>Email: </p>
-                <p>Location: Sydney, Australia</p>
-                <a href="http://">books</a>
-                <button onClick={() => {}}>Upload Resume</button>
-            </header>
+        <div className="container">
+            <div className="left-column">
+                {/* Personal Info */}
+                <div>
+                    <h2>Ayad Farhat</h2>
+                    <p>Software Engineer</p>
+                </div>
+
+                <div>
+                    <p>Email: your@email.com</p>
+                    <p>Phone: +123-456-7890</p>
+                </div>
+
+                {/* Resume Button */}
+                <a
+                    href="path_to_your_resume.pdf"
+                    className="resume-button"
+                    download="Your_Name_Resume"
+                >
+                    Download My Resume
+                </a>
+            </div>
+
+            <div className="main-content">
+                {/* One Page Description */}
+                <h1>About Me</h1>
+                <p>
+                    This is where you can write a detailed description about
+                    yourself, your experience, achievements, and any other
+                    relevant information.
+                </p>
+                <a
+                    href="YOUR_SHEET_LINK_HERE"
+                    className="books-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Books I've Read
+                </a>
+                {/* ... add more content as needed ... */}
+            </div>
         </div>
     );
 }
